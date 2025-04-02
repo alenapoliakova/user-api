b68c6ca5f3888453fd6ba5400f896d6dd5cb0a08
Alena Poliakova <a.polyackowa2016@gmail.com>
2025-04-02 23:37:03 +0300

    Добавлена структура проекта
    Временно убраны проверки mypy
    
    Разделение CI на 2 стадии - test и lint
    
    Первая попытка поправить упавшие тесты
    
    Временно убраны проверки ruff
    
    Итерация 2
    
    Убраны лишние импорты
    
    Итерация 3

---
+++ app/api/v1/api.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+api_router = APIRouter()
+
+# Import and include other routers here
+# Example:
+# from app.api.v1.endpoints import users
+# api_router.include_router(users.router, prefix="/users", tags=["users"])

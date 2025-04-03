cb128b79bebc00479fb1d5ae194e8ea55ba31ad6
Alena Poliakova <a.polyackowa2016@gmail.com>
2025-04-03 23:44:49 +0300

    Добавлена структура проекта, CI, API метод

---
+++ app/db/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+from app.db.base import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String(64), nullable=False)
+    surname = Column(String(64), nullable=False)
+    patronymic = Column(String(64), nullable=True)
+    password_hash = Column(String(255), nullable=False)
+    type = Column(String(16), nullable=False)
+    class_name = Column(String(8), nullable=True)
+    email = Column(String(255), nullable=False, unique=True) 
---
+++ app/schemas/user.py
@@ -0,0 +1,19 @@
+from uuid import UUID
+from pydantic import BaseModel, Field, constr, EmailStr
+
+class UserBase(BaseModel):
+    name: str = Field(..., max_length=64)
+    surname: str = Field(..., max_length=64)
+    patronymic: str | None = Field(None, max_length=64)
+    type: constr(pattern="^(teacher|student|headteacher)$")
+    class_name: str | None = Field(None, max_length=8)
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str = Field(..., min_length=8)
+
+class UserResponse(UserBase):
+    id: UUID
+
+    class Config:
+        from_attributes = True 

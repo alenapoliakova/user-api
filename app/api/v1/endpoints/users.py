65d112901bfdce884469a58f92856accaced526d
daniil-26 <126852384+daniil-26@users.noreply.github.com>
2025-04-21 18:26:02 +0300

    Update users.py

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.base import get_db
 from app.db.models import User
-from app.schemas.user import UserCreate, UserResponse, UserUpdate, UserFilter
+from app.schemas.user import UserCreate, UserFilter, UserResponse, UserUpdate
 
 router = APIRouter()
 
